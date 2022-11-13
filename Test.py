b784a4eb3c4cdd4db8481a65ed56e8f37ce82801
Zaichuan You <zxy456@case.edu>
2022-11-13 01:50:07 -0500

    Utils finished

--- Test.py
+++ Test.py
@@ -8,7 +8,7 @@ birdy_uri = 'spotify:artist:2WX2uTcsvV5OnS0inACecP'
 client = utils.getClient()
 spotify = spotipy.Spotify(client_credentials_manager=client)
 username = "oju0v757td6ecnvja774t086p"
-scope = "playlist-read-private playlist-modify-private user-top-read"
+scope = "playlist-read-private playlist-modify-private playlist-modify-public user-read-private user-read-email"
 token = utils.getToken(username, scope)
 
 """results = spotify.artist_albums(birdy_uri, album_type='album')
@@ -29,17 +29,32 @@ for track in results['tracks'][:10]:
     print('track    : ' + track['name'])
     print('audio    : ' + track['preview_url'])
     print('cover art: ' + track['album']['images'][0]['url'])
-    print()"""
+    print()
 
 
 sp = utils.getAuth(token)
-
+#print(utils.search_song("Dream", spotify))
 
 playlists_u = sp.user_playlists(user=username)
-temp = utils.getSongInPlaylist(playlists_u['items'][0]['uri'], sp)[0]
-print(temp.keys())
+print(utils.get_user_playlist(sp))
+
 temp = utils.encodeJson(temp)
 # Writing to sample.json
 with open("test.json", "w") as outfile:
     outfile.write(temp)
-#utils.getPlayerPlaylistStat(playlists_u, sp)
+utils.getPlayerPlaylistStat(playlists_u, sp)"""
+
+sp = utils.getAuth(token)
+#utils.create_user_playlist(sp)
+#utils.delete_user_playlist(sp)
+playlist_id = ""
+playlists_u = sp.user_playlists(user=utils.get_user_id(sp))['items']
+for playlist in playlists_u:
+    if playlist["name"] == "Partify":
+        playlist_id = playlist["id"]
+song_id = utils.search_song("dream", sp)[0]['id']
+print(song_id)
+#utils.add_song_to_playlist(sp, song_id)
+songs = utils.get_song_In_Playlist(playlist_id, sp)
+song_id = utils.get_songid_by_name(sp, "Dream On")
+utils.remove_song_from_playlist(sp, song_id)
--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ from spotipy import util
 from spotipy.oauth2 import SpotifyClientCredentials
 import pandas as pd
 import pyqrcode
-import png
 from pyqrcode import QRCode
 import json
 
@@ -15,13 +14,13 @@ def getClient():
     return SpotifyClientCredentials(client_id="2c33c1c77b5346438eae177739954110", client_secret="5fd21bbeef534e91ab441448b96493ee")
 
 def getAuth(token):
-    sp = spotipy.Spotify(token)
-    return sp
+    Auth = spotipy.Spotify(token)
+    return Auth
 
-def getSongURIInPlaylist(playlists_uri, sp):
+def get_SongURI_In_Playlist(playlists_uri, Auth):
 
     track = []
-    track.append(sp.playlist_tracks(playlists_uri)['items'])
+    track.append(Auth.playlist_tracks(playlists_uri)['items'])
 
     track_uri = []
     for t in track:
@@ -31,9 +30,9 @@ def getSongURIInPlaylist(playlists_uri, sp):
     
     return track_uri
 
-def getSongInPlaylist(playlists_uri, sp):
+def get_song_In_Playlist(playlists_uri, Auth):
     track = []
-    track.append(sp.playlist_tracks(playlists_uri)['items'])
+    track.append(Auth.playlist_tracks(playlists_uri)['items'])
 
     track_data = []
     for t in track:
@@ -43,29 +42,54 @@ def getSongInPlaylist(playlists_uri, sp):
     
     return track_data
 
-def getPlayerPlaylistStat(playlists, sp):
+def get_user_playlist(Auth):
+    playlists_u = Auth.user_playlists(user=get_user_id(Auth))
+    result = {}
+    for playlist in playlists_u['items']:
+        temp_dict = {}
+        temp_dict['images'] = playlist['images']
+        temp_dict['description'] = playlist['description']
+        result[playlist['name']] = temp_dict
+    
+    return result
+
+def get_songid_by_name(Auth, name):
+    playlist_id = ""
+    playlists_u = Auth.user_playlists(user=get_user_id(Auth))['items']
+    for playlist in playlists_u:
+        if playlist["name"] == "Partify":
+            playlist_id = playlist["id"]
+    songs = get_song_In_Playlist(playlist_id, Auth)
+    song_id = ""
+    for song in songs:
+        if song["name"] == name:
+            song_id = song["id"]
+    
+    return song_id
+
+def get_user_playlist_stat(playlists, Auth):
     playlists_uri = []
     while playlists:
         for i, playlist in enumerate(playlists['items']):
             print("%4d %s %s" % (i + 1 + playlists['offset'], playlist['uri'],  playlist['name']))
             playlists_uri.append(playlist['uri'])
         if playlists['next']:
-            playlists = sp.next(playlists)
+            playlists = Auth.next(playlists)
         else:
             playlists = None
 
     track_uri = []
     for a in playlists_uri:
-        track_uri.append(getSongURIInPlaylist(a, sp))
+        track_uri.append(get_SongURI_In_Playlist(a, Auth))
 
 
-    song_stat = pd.DataFrame(columns=list(sp.audio_features(track_uri[0])[0].keys()))
-    temp = sp.audio_features(track_uri[0])[0]
+    song_stat = pd.DataFrame(columns=list(Auth.audio_features(track_uri[0])[0].keys()))
+    temp = Auth.audio_features(track_uri[0])[0]
     print(temp)
     song_stat = pd.concat([pd.DataFrame(temp, index=[0]), song_stat], axis = 0, ignore_index=True)
 
     for uri in track_uri:
-        song_stat = pd.concat([song_stat, pd.DataFrame(sp.audio_features(uri)[0], index=[0])], axis=0, ignore_index=True)
+        song_stat = pd.concat([song_stat, pd.DataFrame(Auth.audio_features(uri)[0], index=[0])], axis=0, ignore_index=True)
 
     song_stat.drop('track_href', axis=1, inplace=True)
     song_stat.drop('uri', axis=1, inplace=True)
@@ -73,6 +97,69 @@ def getPlayerPlaylistStat(playlists, sp):
     song_stat.drop('type', axis=1, inplace=True)
     song_stat.drop('analysis_url', axis=1, inplace=True)
     return song_stat
+
+
+def drop_Feature(song_Feature):
+    new_stat = {}
+    new_stat['cover_art'] = song_Feature['album']['images']
+    new_stat['artists'] = song_Feature['artists']
+    new_stat['preview'] = song_Feature['preview_url']
+    new_stat['name'] = song_Feature['name']
+    new_stat['id'] = song_Feature['id']
+    new_stat['album_name'] = song_Feature['album']['name']
+    return new_stat
+
+def search_song(song_name, auth):
+    result = auth.search(q=song_name, type="track", limit=10)['tracks']['items']
+    result_list = []
+    for song in result:
+        result_list.append(drop_Feature(song))
+    return result_list
+
+def add_song_to_playlist(Auth, song_id):
+    playlist_id = ""
+    playlists_u = Auth.user_playlists(user=get_user_id(Auth))['items']
+    for playlist in playlists_u:
+        if playlist["name"] == "Partify":
+            playlist_id = playlist["id"]
+    Auth.user_playlist_add_tracks(get_user_id(Auth), playlist_id, [song_id])
+
+def remove_song_from_playlist(Auth, song_id):
+    playlist_id = ""
+    playlists_u = Auth.user_playlists(user=get_user_id(Auth))['items']
+    for playlist in playlists_u:
+        if playlist["name"] == "Partify":
+            playlist_id = playlist["id"]
+    Auth.user_playlist_remove_all_occurrences_of_tracks(get_user_id(Auth), playlist_id, [song_id])
+
+def get_user_id(Auth):
+    """
+    Get id of user which can be use as the only identification of user
+    """
+    return Auth.current_user()["id"]
+
+def get_user_profile(Auth):
+    """
+    Example output: {'country': 'US', 'display_name': 'oju0v757td6ecnvja774t086p', 
+    'email': 'zxy456@case.edu', 'explicit_content': {'filter_enabled': False, 'filter_locked': False}, 
+    'external_urls': {'spotify': 'https://open.spotify.com/user/oju0v757td6ecnvja774t086p'}, 
+    'followers': {'href': None, 'total': 0}, 'href': 'https://api.spotify.com/v1/users/oju0v757td6ecnvja774t086p', 
+    'id': 'oju0v757td6ecnvja774t086p', 'images': [], 'product': 'premium', 'type': 'user', 
+    'uri': 'spotify:user:oju0v757td6ecnvja774t086p'}
+    """
+    profile = Auth.current_user()
+    return profile
+
+def create_user_playlist(Auth):
+    Auth.user_playlist_create(get_user_id(Auth), "Partify", public=True, description="Playlist for the party!")
+
+def delete_user_playlist(Auth):
+    playlist_id = ""
+    playlists_u = Auth.user_playlists(user=get_user_id(Auth))['items']
+    for playlist in playlists_u:
+        if playlist["name"] == "Partify":
+            playlist_id = playlist["id"]
+    Auth.user_playlist_unfollow(get_user_id(Auth), playlist_id)
 
 def createQRcode(url):
     qrCode = pyqrcode.create(url)
